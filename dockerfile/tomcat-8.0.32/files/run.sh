3ff4939c71c836697d27e6b534a8f1f2025e5b00
wzwdev <wzwdev@126.com>
2017-09-01 13:11:50 +0800

    动态修改Tomcat中jvm的内存
    tomcat中的-Xmx参数=容器的总内存-64m内存[预留了监控，故障排查等场景需要的内存缺省64m，其余容器内存我们都分配给JVM的堆
    
    Docker
    1.10提供了对容器资源限制的动态修改能力。但是由于JVM无法感知容器资源修改，我们需要重启tomcat来变更JVM的内存设置，例如，我们可以通过下面命令把容器内存限制调整到1GB
    docker update -m 512m test
    docker restart test
    再次检查日志，相应的JVM Heap Size最大值已被设置为448MB

--- dockerfile/tomcat-8.0.32/files/run.sh
+++ dockerfile/tomcat-8.0.32/files/run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 limit_in_bytes=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
+RESERVED_MEGABYTES=64
 
 # If not default limit_in_bytes in cgroup
 if [ "$limit_in_bytes" -ne "9223372036854771712" ]
@@ -8,7 +9,6 @@ then
     limit_in_megabytes=$(expr $limit_in_bytes \/ 1048576)
     heap_size=$(expr $limit_in_megabytes - $RESERVED_MEGABYTES)
     export JAVA_OPTS="-Xmx${heap_size}m $JAVA_OPTS"
-    echo JAVA_OPTS=$JAVA_OPTS
 fi
 
 /alidata/server/tomcat-8.0.32/bin/startup.sh
