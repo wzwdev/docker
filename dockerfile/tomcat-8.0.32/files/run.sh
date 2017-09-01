4254a1d012912b4d2f493150ad1aff11054eee51
wzwdev <wzwdev@126.com>
2017-09-01 12:14:58 +0800

    动态调整jdk内存

--- dockerfile/tomcat-8.0.32/files/run.sh
+++ dockerfile/tomcat-8.0.32/files/run.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
+limit_in_bytes=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
+
+# If not default limit_in_bytes in cgroup
+if [ "$limit_in_bytes" -ne "9223372036854771712" ]
+then
+    limit_in_megabytes=$(expr $limit_in_bytes \/ 1048576)
+    heap_size=$(expr $limit_in_megabytes - $RESERVED_MEGABYTES)
+    export JAVA_OPTS="-Xmx${heap_size}m $JAVA_OPTS"
+    echo JAVA_OPTS=$JAVA_OPTS
+fi
+
 /alidata/server/tomcat-8.0.32/bin/startup.sh
