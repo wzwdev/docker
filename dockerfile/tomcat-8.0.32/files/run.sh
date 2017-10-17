f49e2882f317b4bc45c5a09341642c9c0349aaa2
wzwdev <wzwdev@126.com>
2017-10-17 14:45:53 +0800

    max memory 1024TB
    为了兼容所有机型，自定义JAVA容器限制的最大内存为1024TB，当小于等于此值时表示用户人为限制了Docker内存,实际上Docker不设置内存限制时limit_in_bytes的值大约为9223372036854775807B，约等于8191TB

--- dockerfile/tomcat-8.0.32/files/run.sh
+++ dockerfile/tomcat-8.0.32/files/run.sh
@@ -3,8 +3,8 @@
 limit_in_bytes=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
 RESERVED_MEGABYTES=64
 
-# If not default limit_in_bytes in cgroup
-if [ "$limit_in_bytes" -ne "9223372036854771712" ]
+# If not default limit_in_bytes in cgroup,wzw define max memory 1024TB
+if [ "$limit_in_bytes" -lte "1125899906842624" ]
 then
     limit_in_megabytes=$(expr $limit_in_bytes \/ 1048576)
     heap_size=$(expr $limit_in_megabytes - $RESERVED_MEGABYTES)
